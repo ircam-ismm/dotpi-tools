ca8a1f3ef55966a1a79408e6a6c14365d1d3a684
jipodine <jipodine@users.noreply.github.com>
2023-12-08 16:20:03 +0100

    dotpi-manager: add log of dotpi_manager_update

--- dotpi_root/share/dotpi-manager/dotpi_manager.bash
+++ dotpi_root/share/dotpi-manager/dotpi_manager.bash
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+# source this file to add functions to the dotpi environment
+
 dotpi_manager_update() {
+  _dotpi_command="$(basename -- "${FUNCNAME[0]:-"$0"}")"
+  log_file="${DOTPI_ROOT}/var/log/${_dotpi_command}_$(date +"%Y%m%d-%H%M%S").log"
+  exec &> >(dotpi log "$log_file")
+
+  dotpi echo_info "Log of dotpi-manager update: ${log_file}"
+
   cd -- "${DOTPI_ROOT}/share/dotpi-manager/runtime"
 
+  service_name='dotpi-manager.service'
+
+  systemctl stop "${service_name}"
+
   git pull origin main
-  rm -Rf node_modules
+  rm -rf node_modules
   npm install
   npm run build
 
-  dotpi_daemon_restart dotpi-manager.service
+  systemctl start "${service_name}"
 }
