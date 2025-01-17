d856b16ed83e8a1636a8f4a95d4390e8479dc6b4
jipodine <jipodine@users.noreply.github.com>
2025-01-17 11:41:18 +0100

    @dotpi/module: add create-config command

---
+++ dotpi_root/dist/@dotpi/module/src/createConfig.js
@@ -0,0 +1,86 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+import { $ } from 'execa';
+
+import * as echo from '@dotpi/javascript/echo.js';
+
+import { dotpiConfigurationGet } from './configuration.js';
+import { definitionGet } from './definition.js';
+
+export function createConfigCommandDefine({program}) {
+  program
+    .command('create-config')
+    .summary('create configuration files for installed modules')
+    .description('Create configuration files installed modules.')
+    .option('-r, --dotpi-root <path>', 'dotpi root path, to initialise environment')
+    .option('-p, --prefix <path>', 'look for installed modules in this directory')
+    .action((modules, options, command) => createConfig(modules, { ...options, command }));
+  ;
+
+  return program;
+}
+
+export async function createConfig({
+  dotpiRoot,
+  prefix,
+} = {}) {
+
+  let installedModulesPath;
+  let installedModulesDefinition;
+  let installedModules;
+  try {
+    if (prefix) {
+      installedModulesPath = prefix;
+    } else {
+      ({ modulesPath: installedModulesPath }
+        = await dotpiConfigurationGet({ dotpiRoot, prefix }));
+    }
+
+    installedModulesDefinition = JSON.parse(
+      await fs.readFile(path.resolve(installedModulesPath, 'package.json'))
+    );
+    installedModules = Object.keys(installedModulesDefinition.dependencies);
+  } catch (error) {
+    echo.error(`Failed to get installed modules: ${error.message}`);
+    process.exit(1);
+  }
+
+  // use array to avoid escaping in command
+  const dotpiRootOption = (dotpiRoot
+    ? ['--dotpi-root', path.resolve(dotpiRoot)]
+    : ''
+  );
+  const projectOption = (prefix
+    ? ['--project', path.resolve(prefix)]
+    : ''
+  );
+
+  // no parallelisation to create config files
+  for (const module of installedModules) {
+    try {
+      const modulePath = path.resolve(installedModulesPath, 'node_modules', module);
+      const moduleDefinition = JSON.parse(await fs.readFile(
+        path.resolve(modulePath, 'package.json')
+      ));
+
+      if (moduleDefinition.scripts['create-config']) {
+        echo.info(`Creating configuration for '${module}' in '${prefix}'`);
+
+        const output = await $({
+          cwd: modulePath,
+          env: { FORCE_COLOR: 'true' }, // do not remove colors
+          verbose: 'full', // print stdout and stderr
+        })`npm run create-config -- ${dotpiRootOption} ${projectOption}`;
+      } else {
+        console.log(`(No configuration file to create for '${module}')`);
+      }
+
+
+    } catch (error) {
+      echo.error(`Failed to create configuration files for '${module}': ${error.message}`, error);
+      process.exit(1);
+    }
+
+  }
+}
