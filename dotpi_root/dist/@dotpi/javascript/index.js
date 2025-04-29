9ace6300864b03e22618a0f06e6ee75b3296e378
jipodine <jipodine@users.noreply.github.com>
2025-04-29 12:35:21 +0200

    @dotpi/javascript: package.jon: exports and main

---
+++ dotpi_root/dist/@dotpi/javascript/index.js
@@ -0,0 +1,6 @@
+export * as bash from './bash.js';
+export * as echo from './echo.js';
+export * as service from './service.js';
+export * as system from './system.js';
+export  * as template from './template.js';
+export * as user from './user.js';
