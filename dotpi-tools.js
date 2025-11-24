8399e0ca4568cf6ed6d020af9da473f0aca08107
jipodine <jipodine@users.noreply.github.com>
2025-11-24 11:25:08 +0100

    dotpi-tools: add option to display version

--- dotpi-tools.js
+++ dotpi-tools.js
@@ -6,18 +6,21 @@ import { greetings } from './src/utils.js';
 import createProject from './src/create-project.js';
 import installRpi from './src/install-rpi.js';
 import configureHost from './src/configure-host.js';
+import { packageVersion } from './src/utils.js';
 
 program
-  .option('--create-project')
-  .option('--install-rpi')
-  .option('--configure-host')
+  .option('--create-project', 'create a new dotpi project')
+  .option('--install-rpi', 'install a dotpi project on a Raspberry Pi SD card')
+  .option('--configure-host', 'configure the host system for dotpi development')
+  .option('-v, --version', 'display version information of this tool')
+  ;
   // .option('--launch-manager');
 
 program.parse();
 
 const options = program.opts();
 
-greetings();
+let displayGreetings = true;
 
 if (Object.keys(options).length === 0) {
   console.log('');
@@ -30,6 +33,9 @@ if (options.createProject) {
   await installRpi();
 } else if (options.configureHost) {
   await configureHost();
+} else if (options.version) {
+  displayGreetings = false;
+  console.log(packageVersion());
 } else if (options.launchManager) {
   console.log(chalk.yellow('> launchManager is not implemented yet'));
 } else {
@@ -38,3 +44,7 @@ if (options.createProject) {
   console.log(options);
 }
 
+
+if (displayGreetings) {
+  greetings();
+}
